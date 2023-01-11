c7ef5fb74296987acedaa65ce77d9ace1f7cb45c
ajitsoni05 <49568689+ajitsoni05@users.noreply.github.com>
2023-01-12 00:57:15 +0530

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,13 @@
+module cheggbot
+
+go 1.18
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	golang.org/x/net v0.5.0
+)
+
+require (
+	github.com/sclevine/agouti v3.0.0+incompatible // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("5641727554:AAGSiVcntJsEtPPMoYAjOAeibxnPD7_eMak")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		_, err := bot.Send(msg)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
